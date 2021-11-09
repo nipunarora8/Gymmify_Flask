a5614b5535e3426fceca8ca7e2b7faa1c7a07dcb
Nipun Arora <46936479+nipunarora8@users.noreply.github.com>
2021-11-10 03:00:27 +0530

    Update dumbell_press_video.py

--- exercises/dumbell_press_video.py
+++ exercises/dumbell_press_video.py
@@ -46,7 +46,7 @@ def dumbell_press(file_path):
     cap = cv2.VideoCapture(file_path)
     
     vid_name=file_path.split('/')[-1]
-    vid_name = '/uploads/'+ vid_name[:-3] + '_out.avi'
+    vid_name = '/uploads/'+ vid_name[:-4] + '_out.avi'
 
     frame_height = int(cap.get(4))
     frame_width = int(cap.get(3))
